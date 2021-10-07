53789135e9ff8bbe550a8d5c86dd6850ed02a912
alexanderalipoyo <alipoyo.alexander@gmail.com>
2021-10-07 17:25:31 +0800

    added sample

--- cypress/integration/examples/Login.spec.js
+++ cypress/integration/examples/Login.spec.js
@@ -1,15 +1,10 @@
 /// <reference types="cypress" />
-
 context('login', () => {
   beforeEach(() => {
-    cy.visit('https://qa-portal.pii-protect.com/#/login')
+    cy.visit('https://portal.pii-protect.com/#/login')
   })
-  it('Login as Partner Admin', () => {
+  it('Browse production URL', () => {
     cy.xpath('//*[@id="email"]')
       .type('Alexander+A@trustsecurenow.com', { force: true });
-    cy.get('.UserLogin__Form-sc-43815n-1').submit();
-    cy.wait(10000)
-    cy.get('#password').should('be.visible').type("Welcome123");
-    cy.xpath('//*[@id="root"]/div[2]/div/main/div/form/div[3]/button/span[1]').click();
   })
 })
